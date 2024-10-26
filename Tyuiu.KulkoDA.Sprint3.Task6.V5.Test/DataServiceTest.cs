9e5ab268aab86cece02471cef76accb2adcc8c05
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-10-26 22:23:28 +0500

    Task0.6

---
+++ Tyuiu.KulkoDA.Sprint3.Task6.V5.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.KulkoDA.Sprint3.Task6.V5.Lib;
+namespace Tyuiu.KulkoDA.Sprint3.Task6.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int i = 15;
+            int k = 22;
+            var res = ds.GetSumTheDivisors(i,k);
+            Assert.AreEqual(,res);
+        }
+    }
+}
---
+++ Tyuiu.KulkoDA.Sprint3.Task6.V5/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.KulkoDA.Sprint3.Task6.V5.Lib;
+namespace Tyuiu.KulkoDA.Sprint3.Task6.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
